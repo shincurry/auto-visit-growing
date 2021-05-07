4a41eb526685ebc86337843054db10e8800d91ca
ShinCurry <shincurryyang@gmail.com>
2021-05-07 12:10:01 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM node:16-alpine
 
+RUN apk add --update --no-cache chromium
+
+# Do not use puppeteer embedded chromium
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
+ENV CHROMIUM_PATH="/usr/bin/chromium-browser"
+
 WORKDIR /usr/src/app
 
 COPY . .
--- index.js
+++ index.js
@@ -1,60 +1,34 @@
 const schedule = require('node-schedule');
-const axios = require('axios');
-const { DateTime } = require('luxon');
+const puppeteer = require('puppeteer-core');
 
 if (!process.env.TARGET_WALLET) {
 	throw new Error("please set TARGET_WALLET env.")
 }
-
 const TARGET_WALLET = process.env.TARGET_WALLET;
 
-async function requestHistoryHoldings(address, startAt) {
-	const { data } = await axios.post(`https://api-sg.growingfi.com/v2/gql`, {
-		query: `
-			query($addresses: [ID!]!, $startAt: String) {
-        holdingsHistory(address: $addresses, startAt: $startAt) {
-          holdings
-          time
-        }
-      }
-		`,
-		variables: {
-      addresses: [address],
-      startAt,
-    }
-	})
-	// console.log({data})
-	console.log(`requsted HistoryHoldings for wallet [${TARGET_WALLET}] at ${new Date().toLocaleString()}`)
-}
-async function requestHistoryPrices(address, startAt) {
-	const { data } = await axios.post(`https://api-sg.growingfi.com/v2/gql`, {
-    query: `
-      query($tokens: [ID!]!, $startAt: String) {
-        priceHistory(tokens: $tokens, startAt: $startAt) {
-          price
-          time
-        }
-      }
-    `,
-    variables: {
-      tokens: [address],
-      startAt,
-    }
-  })
-	// console.log({data})
-	console.log(`requsted HistoryPrices for wallet [${TARGET_WALLET}] at ${new Date().toLocaleString()}`)
-}
-async function requestHoldingsData(address) {
-	const startAt = DateTime.local().plus({ hours: -2 }).startOf('hour').toJSDate()
-	await Promise.all([
-		requestHistoryHoldings(address, startAt),
-		requestHistoryPrices(address, startAt),
-	])
+
+async function visitGrowingWebsite(browser) {
+	const page = await browser.newPage();
+  await page.goto(`https://www.growing.fi/wallet/${TARGET_WALLET}`);
+  console.log(`visited growing.fi for wallet [${TARGET_WALLET}] at ${new Date().toLocaleString()}`)
+  setTimeout(() => {
+    page.close();
+  }, 30 * 1000)
 }
 
-requestHoldingsData(TARGET_WALLET)
-const job = schedule.scheduleJob('0 */10 * * * *', function() {
-	requestHoldingsData(TARGET_WALLET)
-});
+async function run() {
+  const browser = await puppeteer.launch({
+    userDataDir: '/user_data',
+    executablePath: process.env.CHROMIUM_PATH,
+    args: ['--no-sandbox'],
+  });
+  
+  visitGrowingWebsite(browser)
+  const job = schedule.scheduleJob('0 */10 * * * *', function() {
+    visitGrowingWebsite(browser)
+  });
+
+}
 
 
+run()
